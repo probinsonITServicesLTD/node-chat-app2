74f0acb2e9b558e7e533e4efdbede921e34e249e
Paul Robinson <probi@AsusLaptop.its.ie>
2018-09-19 09:42:29 +0100

    Added sockets

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port ,()=>{
+io.on('connection', (socket)=>{
+    console.log('New user connected ', socket.id);
+
+    socket.on('disconnect', (socket)=>{
+        console.log('Client disconnected ', socket.id);
+    });
+});
+
+
+
+server.listen(port ,()=>{
     console.log(`Server runing on port ${port}`);
 })
 
